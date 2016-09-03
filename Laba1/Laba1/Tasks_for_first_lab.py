01c79915c2768568edff4a2e2e439c1244d9379e
Юрий Савицкий < >
2016-09-03 23:48:35 +0700

    сделано пятое задание

--- Laba1/Laba1/Tasks_for_first_lab.py
+++ Laba1/Laba1/Tasks_for_first_lab.py
@@ -2,23 +2,38 @@ from sympy import *
 
 def Task_1_solve_the_equation(): 
     x = Symbol('x') 
-    rooot=solve((x**3)+3*(x**2)-10*x+5, x) 
-    print(rooot) 
+    root=solve((x**3)+3*(x**2)-10*x+5, x) 
+    print("The solve of the equation: ",root) 
 
 def Task_2_work_with_file(filename): 
     filef=open(filename, 'r') 
     allFile=filef.read() 
     filef.close() 
 
-    print(allFile) 
+    print("Text from file:", allFile) 
 
     filef=open(filename, 'w') 
     filef.write(allFile+allFile) 
     filef.close()
 
-def Task_4_find_derivarive():
+def Task_4_find_derivative():
     x=Symbol('x') 
     y=Symbol('y')
     expr=sin(x)*cos(x**2)*tan(y)+ln(x)*sin(x)*cos(x**2)*tan(y)+ln(x)
     expr_dif=diff(expr, x) 
-    print(simplify(expr_dif))
+    
+    print("Derivative: ", simplify(expr_dif))
+
+def Task_5_solve_integrals():
+    x=Symbol('x') 
+    y=Symbol('y') 
+
+    func1=x*ln(x)
+    func2=sin(x)/(x+1)
+
+    undef_integr = integrate(func1, x)
+
+    def_integr = integrate(func2,(x, 0, 1))
+
+    print("Undefind integral :", undef_integr)
+    print("Defind integral :", def_integr)
--- Laba1/Laba1/main.py
+++ Laba1/Laba1/main.py
@@ -1,6 +1,9 @@
 import Tasks_for_first_lab as taskI
 
-print("Don't write anithing!")
 taskI.Task_1_solve_the_equation()
+print("\n")
 taskI.Task_2_work_with_file('text.txt')
-taskI.Task_4_find_derivarive()
+print("\n")
+taskI.Task_4_find_derivative()
+print("\n")
+taskI.Task_5_solve_integrals()
