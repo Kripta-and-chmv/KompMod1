756d5ea5b09aa73bd37018e557761559bd8c024d
Юрий Савицкий < >
2016-09-03 23:32:35 +0700

    сделано 4ое задание

--- Laba1/Laba1/Tasks_for_first_lab.py
+++ Laba1/Laba1/Tasks_for_first_lab.py
@@ -1,8 +1,8 @@
-import sympy 
+from sympy import *
 
 def Task_1_solve_the_equation(): 
-    x = sympy.Symbol('x') 
-    rooot=sympy.solve((x**3)+3*(x**2)-10*x+5, x) 
+    x = Symbol('x') 
+    rooot=solve((x**3)+3*(x**2)-10*x+5, x) 
     print(rooot) 
 
 def Task_2_work_with_file(filename): 
@@ -15,3 +15,10 @@ def Task_2_work_with_file(filename):
     filef=open(filename, 'w') 
     filef.write(allFile+allFile) 
     filef.close()
+
+def Task_4_find_derivarive():
+    x=Symbol('x') 
+    y=Symbol('y')
+    expr=sin(x)*cos(x**2)*tan(y)+ln(x)*sin(x)*cos(x**2)*tan(y)+ln(x)
+    expr_dif=diff(expr, x) 
+    print(simplify(expr_dif))
--- Laba1/Laba1/main.py
+++ Laba1/Laba1/main.py
@@ -1,5 +1,6 @@
 import Tasks_for_first_lab as taskI
-import sympy
 
 print("Don't write anithing!")
+taskI.Task_1_solve_the_equation()
 taskI.Task_2_work_with_file('text.txt')
+taskI.Task_4_find_derivarive()
